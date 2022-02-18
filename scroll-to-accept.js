6e3847cc2c53d9e3423e20fec142ff706032b39c
Anwen Williams <anwen@anwenwilliams.com>
2022-02-18 23:42:35 +0000

    Replace event listener with intersection observer

--- scroll-to-accept.js
+++ scroll-to-accept.js
@@ -1,13 +1,28 @@
 function scrollToAccept() {
     const terms = document.querySelector('.terms-and-conditions');
+    const watch = document.querySelector('.watch'); // element to observe
+    const button = document.querySelector('.accept');
 
     if (!terms) {
         return; // quit if there isn't this item on page
     }
 
-    terms.addEventListener('scroll', function (e) {
-        console.log(e.currentTarget);
+    // new intersection observer with callback function
+    function obCallback(payload) {
+        if (payload[0].intersectionRatio === 1) { 
+            button.disabled = false; // un-disable the accept button
+            ob.unobserve(terms.lastElementChild);  // stop observing terms element
+        }
+    }
+
+    const ob = new IntersectionObserver(obCallback, {
+        root: terms,
+        threshold: 1 // fully on screen
     });
+   
+    // tell intersection observer to watch something
+    ob.observe(terms.lastElementChild);
+
 }
   
 scrollToAccept();
